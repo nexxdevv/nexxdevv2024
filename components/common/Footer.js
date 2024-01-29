f1913a52ab2902bbcd911bdeff7111c261e07953
csx2549 <sanchez.cliff.86@gmail.com>
2024-01-28 18:28:23 -0800

    fix footer darkmode

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='max-w-6xl px-4 py-2 mx-auto text-[11px] text-center '>
-        <p className="opacity-50">&copy; 2024 @nexxdevv</p>
+        <p className="opacity-50 dark:text-white">&copy; 2024 @nexxdevv</p>
     </footer>
   )
 }
